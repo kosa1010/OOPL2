4a1d55e76c66b4f620bae8e6bd07ff8b18ae2983
Dawid <kosiordawid@gmail.com>
2023-03-06 07:29:15 +0100

    init

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -159,10 +159,10 @@ public class Tests {
         InputStream in = new ByteArrayInputStream(input.getBytes());
         Zad3 z = new Zad3(new Scanner(in));
 
-        double[] a = {6, 7, 8, 9, 0, 1, 2, 3, 4};
-        double[] b = {4, 3, 2, 1, 0, 9, 8, 7, 6};
-        double[] c = {6, 8, 0, 2, 4};
-        double[] d = {7, 9, 1, 3};
+        double[] a = {5, 6, 7, 8, 9, 0, 1, 2, 3, 4};
+        double[] b = {4, 3, 2, 1, 0, 9, 8, 7, 6, 5};
+        double[] c = {5, 7, 9, 1, 3};
+        double[] d = {6, 8, 0, 2, 4};
 
         Assert.assertEquals(a, z.table());
         Assert.assertEquals(b, z.invertTable());
